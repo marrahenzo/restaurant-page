bed8ffc026b978052c5fdfd63b54bd51ec07c64a
Hernán Marrapodi <hernanmarra2002@gmail.com>
2022-06-10 18:04:44 -0300

    Add eventlisteners to navbar buttons

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import loadPage from './page_load.js';
+import loadHome from './home.js';
+import loadMenu from './menu.js';
+import loadContact from './contact.js';
 
 loadPage();
 
 const page = document.querySelector('#content');
+const main = document.querySelector('#main');
 const homeButton = document.querySelector('#home');
 const menuButton = document.querySelector('#menu');
 const contactButton = document.querySelector('#contact');
+
+homeButton.addEventListener('click', () => {
+  main.innerHTML = '';
+  loadHome();
+});
+
+menuButton.addEventListener('click', () => {
+  main.innerHTML = '';
+  loadMenu();
+});
+
+contactButton.addEventListener('click', () => {
+  main.innerHTML = '';
+  loadContact();
+});
