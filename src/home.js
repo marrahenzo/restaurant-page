cab552e20bb649870a400c40e0d75f04a3ff69a7
Hernán Marrapodi <hernanmarra2002@gmail.com>
2022-06-10 19:57:02 -0300

    Redo DOM content and reappend

--- src/home.js
+++ src/home.js
@@ -1,16 +1,26 @@
 export default function loadHome() {
   const main = document.querySelector('#main');
 
-  const descripcion = document.createElement('p');
-  descripcion.textContent =
-    'El restaurante "El Viejo Marino"en el balnerio Reta, Provincia de Buenos Aires, Argentina.';
+  const description = [
+    document.createElement('p'),
+    document.createElement('p'),
+    document.createElement('p'),
+  ];
+  description[0].textContent =
+    'PizzaPodi provides the tastiest pizzas in the region';
 
-  const especialidades = document.createElement('p');
-  especialidades.textContent =
-    'Especialidad: Mariscos, pescados, carnes grilladas, pastas caseras, postres de cocina, cafetería.';
+  description[1].textContent =
+    'With decades of experience in our hands, we employ some of the best chefs, so that you can enjoy the best food';
+
+  description[2].textContent =
+    'Ask us about our daily specialties and promos! We may even give you some extras if you ask nicely ;)';
+
+  const image = document.createElement('img');
+  image.src = '../media/home.jpg';
+  image.style.width = '500px';
 
   const slogan = document.createElement('p');
-  slogan.textContent = 'Más de una década disfrutando juntos de Reta...';
+  slogan.textContent = 'Your best pizza, since 1998';
 
-  main.append(descripcion, especialidades, slogan);
+  main.append(...description, image, slogan);
 }
