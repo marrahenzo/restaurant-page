9c50268f3f3152fc280dcdd3156857c567bb330a
Hernán Marrapodi <hernanmarra2002@gmail.com>
2022-06-10 17:17:43 -0300

    Add buttons for navbar items

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import loadPage from './page_load.js';
 loadPage();
 
 const page = document.querySelector('#content');
+const homeButton = document.querySelector('#home');
+const menuButton = document.querySelector('#menu');
+const contactButton = document.querySelector('#contact');
