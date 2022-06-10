626639d7cabded340ffce93b0d6abd9bc7cfe697
Hernán Marrapodi <hernanmarra2002@gmail.com>
2022-06-10 16:15:36 -0300

    Add main function for loading basic page structure

--- src/index.js
+++ src/index.js
@@ -3,6 +3,3 @@ import loadPage from './page_load.js';
 loadPage();
 
 const page = document.querySelector('#content');
-let title = document.createElement('h1');
-title.textContent = 'Hello!';
-page.appendChild(title);
--- src/page_load.js
+++ src/page_load.js
@@ -1,20 +1,44 @@
 export default function loadPage() {
   //Create DOM elements
   const content = document.querySelector('#content');
+
   const nav = document.createElement('div');
-  const background = document.createElement('div');
-  const main = document.createElement('div');
-  const footer = document.createElement('div');
+  nav.id = 'navbar';
   const navTabs = [
     document.createElement('a'),
     document.createElement('a'),
     document.createElement('a'),
+    document.createElement('a'),
   ];
-  nav.id = 'navbar';
+  navTabs[0].id = 'logo';
+  const logoImage = document.createElement('img');
+  logoImage.src = '../media/logo.png';
+  navTabs[1].id = 'home';
+  navTabs[1].id = 'home';
+  navTabs[2].id = 'menu';
+  navTabs[2].id = 'menu';
+  navTabs[3].id = 'contact';
+  navTabs[3].id = 'contact';
+
+  const background = document.createElement('div');
   background.id = 'background';
+
+  const main = document.createElement('div');
   main.id = 'main';
+
+  const footer = document.createElement('div');
   footer.id = 'footer';
+  const footerCredits = document.createElement('a');
+  footerCredits.textContent = '2022 © Hernán Marrapodi';
+  const gitHubLogo = document.createElement('img');
+  gitHubLogo.src = '../media/github-logo.png';
+  gitHubLogo.width = 25;
+
   //Append elements
-  background.appendChild(main);
+  navTabs[0].append(logoImage);
+  nav.append(...navTabs);
+  background.append(main);
+  footerCredits.append(gitHubLogo);
+  footer.append(footerCredits);
   content.append(nav, background, footer);
 }
